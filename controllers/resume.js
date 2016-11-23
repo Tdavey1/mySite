14edd3586c567c898b1c4c7145da9e090b790690
Taira Davey <td284@cornell.edu>
2016-11-23 01:40:40 -0500

    add org name for activities

--- controllers/resume.js
+++ controllers/resume.js
@@ -77,55 +77,9 @@ angular.module("personalSite")
 			timeFrame: "Sept. 2013-May 2016"
 		},
 		Taekwondo: {
+			organization: "Taekwondo club",
 			location: "Cornell University, Ithaca, NY",
 			timeFrame: "Sept. 2013-May 2016"
 		}
 	}
-
-/*
-Activities
-
-Red Cross club	 Sept. 2013-May 2016
-Cornell University, Ithaca, NY
-•	Secretary Sept. 2015 – May 2016
-•	Blood drives, Disaster Action Team, community work
-
-Taekwondo club Sept. 2013-May 2016
-Cornell University, Ithaca, NY
-
-About Me
-I switched to web development from pre-med, because I enjoy coding more than anything I’ve done previously. Since I began programming, I have been very excited about the work that I am doing and will continue to do. People say that I am both a quick learner and highly motivated to improve. 
-
-Software Experience
-
-
-Project Manager		           Summer/Fall 2016
-Psychology department, Queens College, Queens, NY
-•	Managed a team to develop an app for a longitudinal birth cohort study’s data entry
-•	Used HTML, Bootstrap, and AngularJS for the front end
-•	Used NodeJS, and MySQL for the back end
-
-Other Experience
-
-Research Assistant		  Spring 2015 – Spring 2016
-Entomology Department, Cornell University, Ithaca, NY
-•	Studied population dynamics of Musca Domesica
-•	DNA extraction, PCR, Gel electrophoresis, DNA cloning
-
-A Broader View			                 	Winter 2015
-La Ceiba, Honduras
-•	Assisted dentists in a dental clinic
-•	Organize patient files
-
-Research Assistant		        Summers 2012/2013
-Psychology department, Queens College, Queens, NY
-•	Studied effects of stress on children of pregnant women for longitudinal birth cohort study
-•	Patient recruitment
-•	Tissue powderization and DNA extraction 
-
-Publications
-Meisel et al. 2016. Is Multifactorial Sex Determination in the House Fly, Musca domestica (L.), Stable Over Time? Journal of Heredity 107 (7): 615-625 
-
-
-*/
 });
